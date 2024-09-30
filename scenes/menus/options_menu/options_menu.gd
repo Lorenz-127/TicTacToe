a747c5834b5419e3dc3dbbe0aa42b6cce11f77dc
Lorenz B. <lorenz.baechtold@web.de>
2024-10-01 00:33:57 +0200

    feat(ui): add game mode selection to options menu
    - Create new options_menu scene with game mode selection buttons
    - Add "Player vs Player" and "Player vs AI" buttons
    - Implement back button to return to main menu
    - Set up initial signals for game mode changes and menu navigation

---
+++ scenes/menus/options_menu/options_menu.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+
+signal pvp_mode
+signal pve_mode
+signal back_to_main
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pve_button_pressed():
+	pve_mode.emit()
+
+
+func _on_back_button_pressed():
+	back_to_main.emit()
+
+
+func _on_pvp_button_pressed():
+	pvp_mode.emit()
