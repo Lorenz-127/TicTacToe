18308fb7446bb83536820034759ee2c57fd102fa
Lorenz B. <lorenz.baechtold@web.de>
2024-10-03 18:52:36 +0200

    refactor(options): Remove unused buttons
    - Remove PVE Button
    - Remove PVP Button
    - Remove Signal variables for buttons
    - Remove functions for button

--- scenes/menus/options_menu/options_menu.gd
+++ scenes/menus/options_menu/options_menu.gd
@@ -1,10 +1,7 @@
 extends CanvasLayer
 
 
-signal pvp_mode
-signal pve_mode
 signal back_to_main
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,14 +13,5 @@ func _ready():
 func _process(_delta):
 	pass
 
-
-func _on_pve_button_pressed():
-	pass
-
-
 func _on_back_button_pressed():
 	back_to_main.emit()
-
-
-func _on_pvp_button_pressed():
-	pass
